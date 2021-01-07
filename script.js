fc264c7e0f0213f483141e13ba4a7b6359c23882
Roberto C Melchor <rmelchor151@gmail.com>
2021-01-07 12:54:37 -0500

    complete 8 - 13

--- script.js
+++ script.js
@@ -14,7 +14,17 @@ document.getElementsByClassName("footer")[0].classList.add("main")
 //7) Remove the class main on the div with a class of footer.
 document.getElementsByClassName("footer")[0].classList.remove("main")
 //8) Create a new li element.
+let liElement = document.createElement("li")
 //9) Give the li the text "four".
+let liText = document.createTextNode("four")
+liElement.append(liText)
 //10) Append the li to the ul element.
+document.getElementsByTagName("UL")[0].appendChild(liElement)
 //12) Loop over all of the lis inside the ol tag and give them a background color of "green".
+let olList = document.getElementsByTagName("OL")[0].getElementsByTagName("li")
+for (const index in olList){
+    olList[index].style.backgroundColor = "green"
+}
 //13) Remove the div with a class of footer.
+let footer = document.getElementsByClassName("footer")[0]
+document.body.removeChild(footer)
