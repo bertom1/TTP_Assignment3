ef872ce5d460f4a09595f09cd09fba5b10f7e493
Roberto C Melchor <rmelchor151@gmail.com>
2021-01-07 12:19:37 -0500

    complete 3 - 7

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ document.getElementById("container")
 //2) Select the section with an id of container using querySelector.
 document.querySelector("#container")
 //3) Select all of the list items with a class of "second".
+document.getElementsByClassName("second")
 //4) Select a list item with a class of third, but only the list item inside of the ol tag.
+document.getElementsByTagName("OL")[0].getElementsByClassName("third")
 //5) Give the section with an id of container the text "Hello!".
+document.getElementById("container").innerHTML = "<p>Hello!</p>"
 //6) Add the class main to the div with a class of footer.
+document.getElementsByClassName("footer")[0].classList.add("main")
 //7) Remove the class main on the div with a class of footer.
+document.getElementsByClassName("footer")[0].classList.remove("main")
 //8) Create a new li element.
 //9) Give the li the text "four".
 //10) Append the li to the ul element.
